6972258549c00ffbb1d0227a64107aefb95ab8cf
Gurnelius Terer <terergurnelius@gmail.com>
2024-09-19 16:11:38 +0300

    added product images

--- products/models.py
+++ products/models.py
@@ -9,6 +9,12 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_sellable = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
     
     def __str__(self):
         return self.name
+
+    def get_image_url(self):
+        if self.image:
+            return self.image.url
+        return 'product_default.jpg'
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import ProductListView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('products/', ProductListView.as_view(), name='product_list'),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
